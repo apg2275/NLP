0a1e26da4a6b2482f56aec8c0ff764e83056c69e
apg2275 <apg2275@utexas.edu>
2021-09-13 20:00:53 -0500

    Coding part done, comments needed to be added still

--- hw3.R
+++ hw3.R
@@ -3,4 +3,24 @@ library(quanteda.textstats)
 
 data <- read.csv("datasets/datasets/gop_debates.csv")
 
-textstat_readability(data$text[c(4,13)],measure = "Flesch.Kincaid")
+data$text[c(50,100)]
+
+data <- data %>% 
+  bind_cols(textstat_readability(.$text,measure = c("Flesch")))
+library(quanteda)
+corpus <- data %>% corpus(docid_field = "doc_id", unique_docnames = TRUE)
+corpus_tokens <- corpus %>% tokens()
+corpus_dfm <- corpus_tokens %>% dfm(remove = stopwords('en'))
+
+
+
+rankings <- data %>% 
+    group_by(who) %>% 
+    summarise(ave_readability = mean(Flesch))
+rankings$ave_readability <- as.numeric(rankings$ave_readability)
+
+rank <- order(rankings$ave_readability, decreasing = FALSE)
+
+rankings <- rankings[rank,]
+
+rankings
