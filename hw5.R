9f191938a9913b79686561536ba15fb79e820767
apg2275 <apg2275@utexas.edu>
2021-09-27 19:35:49 -0500

    Implemented TF-IDF analysis to all indivudal GOP debates

--- hw5.R
+++ hw5.R
@@ -1,6 +1,7 @@
 
 # Load library 
 library(tidyverse)
+library(tidytext)
 
 setwd("datasets/datasets/gop_frags/")
 
@@ -21,5 +22,35 @@ df1 <- gop_df %>%
 textNoSpeaker <- gsub(".*:","",df1$text)
 
 df1$text <- textNoSpeaker
+
+data_word_n <- df1 %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words) %>%
+  count(date, word, sort = TRUE)
+
+total_words <- data_word_n %>% 
+  group_by(date) %>% 
+  summarize(total = sum(n))
+
+data_word_n <- left_join(data_word_n, total_words)
+
+data_word_n <- data_word_n %>%
+  bind_tf_idf(word, date, n)
+
+data_word_n %>%
+  select(-total) %>%
+  arrange(desc(tf_idf))
+
+data_word_n %>%
+  arrange(desc(tf_idf)) %>%
+  mutate(word = factor(word, levels = rev(unique(word)))) %>% 
+  group_by(date) %>% 
+  slice(1:10) %>% 
+  ungroup() %>%
+  ggplot(aes(word,tf_idf, fill = date)) +
+  geom_col(show.legend = FALSE) +
+  labs(x = NULL, y = "tf-idf") +
+  facet_wrap(~date, ncol = 2, scales = "free") +
+  coord_flip()
 
 
