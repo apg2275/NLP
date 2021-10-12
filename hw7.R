544588bb97a7931ad49516bc1f73bcadef2d99ca
apg2275 <apg2275@utexas.edu>
2021-10-11 22:14:29 -0500

    Added dabest graph, and 95% CI for analysis and comments to code

--- hw7.R
+++ hw7.R
@@ -1,22 +1,23 @@
 library(tidyverse)
 library(tidytext)
 library(textstem)
-library(fuzzyjoin)
 library(dabestr)
 
+# Importing GOP data, remove speaker name, and selects candidates 
+# useful for the research question
+data <- read.csv("datasets/datasets/gop_debates.csv") %>%
+  mutate(text=str_replace(text,"^.*?(?=:): ","")) %>% 
+  filter(who %in% c("TRUMP","BUSH","CRUZ", "FIORINA")) 
 
-data <- read.csv("datasets/datasets/gop_debates.csv")
-
-data$text <- gsub(".*:","",data$text)
-
+# Selects only the fear words in the nrc dictionary
 nrc_fear <- get_sentiments("nrc") %>%
   filter(sentiment == "fear")
 
+# Adds a label to turn for easier data handling later
 data$turn <- c(1:length(data$text))
 
-df1<-data[(data$who=="TRUMP" | data$who=="CRUZ" | data$who=="BUSH" | data$who=="FIORINA"),]
-
-nrc_analysis <- df1 %>% 
+# Collects fear word(s) count for each speaking turn
+nrc_analysis <- data %>% 
   unnest_tokens(word, text, token = "words") %>% 
   regex_inner_join(nrc_fear) %>% 
   group_by(turn, word.x) %>% 
@@ -25,9 +26,23 @@ nrc_analysis <- df1 %>%
   group_by(turn) %>% 
   summarise(fear_count = n())
 
-df1_nrc <- df1 %>% left_join(nrc_analysis)
+# Attaches fear word count to dataset
+df1_nrc <- data %>% left_join(nrc_analysis)
 
+# Collects mean fear word count for each candidate
 df1_nrc %>% group_by(who) %>% filter(!is.na(fear_count)) %>% summarise(count=mean(fear_count))
 
+# Setting up dabest for mean difference testing
+nrc_dabest <- df1_nrc %>% select(who, fear_count) %>% 
+  filter(!is.na(fear_count)) %>% 
+  dabest(x = who,
+         y= fear_count,
+         idx= c("TRUMP","BUSH", "CRUZ", "FIORINA"),
+         paired = FALSE)                                       
 
+# Displays mean difference bootstrap with 95% CI compared to Trump
+nrc_dabest %>% mean_diff()
+
+# Plots the mean difference compared to Trump
+nrc_dabest %>% mean_diff() %>% plot()
 
